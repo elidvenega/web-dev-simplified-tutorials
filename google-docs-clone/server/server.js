591888c0666afede709771898086c0a0ececac83
elid venega <venegaelid@gmail.com>
2021-08-24 15:59:59 -0700

    Made Google docs clone work

--- google-docs-clone/server/server.js
+++ google-docs-clone/server/server.js
@@ -1,8 +1,13 @@
-const io = require('socket.io')(3001,{
+const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
-        methods: ["GET","POST"],
+        methods: ["GET", "POST"],
     },
 })
 
-io.on("connection", socket => {})
+io.on("connection", socket => {
+        socket.on("send-changes", delta => {
+        socket.broadcast.emit("receive-changes", delta)
+        })
+    // console.log("connected");
+})
