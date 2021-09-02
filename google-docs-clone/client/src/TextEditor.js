eb85f356c97e18c7b4bcf5cca64b30f94935d46f
elid venega <venegaelid@gmail.com>
2021-09-02 15:11:37 -0700

    Work on google docs project

--- google-docs-clone/client/src/TextEditor.js
+++ google-docs-clone/client/src/TextEditor.js
@@ -3,6 +3,7 @@ import { useCallback, useEffect,useState } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,7 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const {id: documentId} = useParams();
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
 
@@ -27,6 +29,17 @@ export default function TextEditor() {
     s.disconnect()
     }
     },[])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        socket.once("loaded-document", document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId)
+    }, [socket,quill,documentId])
 
     useEffect(() => {
         if (socket == null || quill == null) return
@@ -63,6 +76,8 @@ export default function TextEditor() {
         wrapper.append(editor);
         const q = new Quill(editor, { theme: 'snow', 
         modules: { toolbar: TOOLBAR_OPTIONS }});
+        q.enable(false)
+        q.setText("Loading...")
         setQuill(q)
     }, [])    
     return (
