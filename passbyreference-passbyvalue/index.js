7f49c51b6c1bc00c689b7efccf7fbae38a629c32
elidvenega <venegaelid@gmail.com>
2022-08-25 12:17:45 -0700

    worked on values and reference

---
+++ passbyreference-passbyvalue/index.js
@@ -0,0 +1,18 @@
+// let a = [1, 2, 3, 4];
+
+// let b = a;
+
+// b = [2,4,6]
+
+// console.log(a);
+// console.log(b);
+
+let a = 10;
+let b = "Hi";
+let c = [1,2];
+let d = c;
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
+console.log(`c = ${c}`);
+console.log(`d = ${d}`);
